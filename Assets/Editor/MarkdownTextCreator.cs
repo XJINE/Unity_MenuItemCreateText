2b9fe25a20372947206eab5f1c940fe390aa5163
XJINE <xjine.andorelseif@gmail.com>
2018-03-23 21:54:57 +0900

    First commit.

---
+++ Assets/Editor/MarkdownTextCreator.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MarkdownTextCreator
+{
+    [MenuItem("Assets/Create/Markdown")]
+    public static void Create()
+    {
+        // NOTE:
+        // Following code doesn't works well.
+        //
+        // TextAsset textAsset = new TextAsset();
+        // if (textAsset == null)
+        // {
+        //     Debug.Log("textAsset is null.");
+        // }
+        // AssetDatabase.CreateAsset(textAsset, "MarkdownText.md");
+
+        string assetPath = GetCurrentAssetDirectoryPath();
+
+        assetPath = assetPath + "/MarkdownText.md";
+
+        System.IO.File.WriteAllText(assetPath, "");
+
+        AssetDatabase.Refresh();
+
+        Object asset = AssetDatabase.LoadAssetAtPath<Object>(assetPath);
+
+        StartSelectedAssetRename(asset);
+    }
+
+    public static string GetCurrentAssetDirectoryPath()
+    {
+        string assetPath = Application.dataPath;
+
+        foreach (Object obj in Selection.GetFiltered(typeof(Object), SelectionMode.Assets))
+        {
+            assetPath = AssetDatabase.GetAssetPath(obj);
+
+            if (System.IO.File.Exists(assetPath))
+            {
+                assetPath = System.IO.Path.GetDirectoryName(assetPath);
+                break;
+            }
+        }
+
+        return assetPath;
+    }
+
+    public static void StartSelectedAssetRename(Object asset)
+    {
+        EditorWindow projectWindow = EditorWindow.GetWindow
+            (typeof(EditorWindow).Assembly.GetType("UnityEditor.ProjectBrowser"));
+
+        Selection.activeObject = asset;
+
+        EditorUtility.FocusProjectWindow();
+
+        projectWindow.SendEvent(Event.KeyboardEvent(KeyCode.F2.ToString()));
+    }
+
+    private static void CheckUnity3DWindowNames()
+    {
+        // NOTE:
+        // This function is used to check Unity window name. So mainly for debug.
+
+        EditorWindow[] editorWindows = Resources.FindObjectsOfTypeAll<EditorWindow>();
+
+        foreach (EditorWindow editorWindow in editorWindows)
+        {
+            Debug.Log(editorWindow.GetType().ToString());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Unity3D_MarkdownTextCreator
-Unity に Asset/Create/Markdown を追加します。
+# Unity_MarkdownTextCreator
+
+Add "Assets/Create/Markdown" menu to Unity.
+
+![](https://github.com/XJINE/Unity_MarkdownTextCreator/blob/master/screenshot.png)
+
+## Import to Your Project
+
+If you want to import this repository as submodule in your project.
+
+```
+git submodule add -b submodule https://github.com/XJINE/Unity_MarkdownTextCreator.git Assets/Packages/MarkdownTextCreator
+```
